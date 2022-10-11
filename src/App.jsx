61c10133a503c8bb37e97c83eafb084b8da9039b
tiny tony <tintony1@outlook.com>
2022-10-11 20:12:17 +0300

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,210 @@
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages";
-import Auth from "./pages/auth";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-import Home from "./pages/home";
+import {
+  Typography,
+  Container,
+  AppBar,
+  Toolbar,
+  Avatar,
+  Menu,
+  MenuItem,
+  Stack,
+  Button,
+  Modal,
+  TextField,
+} from "@mui/material";
+import { useReducer, useState } from "react";
 
 import "./App.css";
-import Profile from "./pages/profile";
+
+// reducer
+const initialState = {
+  openModal: false,
+  openMenu: false,
+};
+
+function LocalReducer(state, action) {
+  switch (action.type) {
+    case "openModal":
+      return {
+        ...state,
+        openModal: true,
+      };
+    case "closeModal":
+      return {
+        ...state,
+        openModal: false,
+      };
+    case "openMenu":
+      return {
+        ...state,
+        openMenu: action.payload,
+      };
+    case "closeMenu":
+      return {
+        ...state,
+        openMenu: null,
+      };
+
+    default:
+      return state;
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(LocalReducer, initialState);
+
+  function OpenModal() {
+    dispatch({ type: "openModal" });
+  }
+  function CloseModal() {
+    dispatch({ type: "closeModal" });
+  }
+  function OpenMenu(event) {
+    dispatch({ type: "openMenu", payload: event.currentTarget });
+  }
+  function CloseMenu() {
+    dispatch({ type: "closeMenu" });
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route index element={<Home />} />
-        <Route path="user/profile" element={<Profile />} />
-      </Route>
-      <Route path="/auth" element={<Auth />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-    </Routes>
+    <Container>
+      <AppBar sx={{ position: "sticky" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h5">AuthSite</Typography>
+          <Avatar sx={{ cursor: "pointer" }} onClick={OpenMenu} />
+          <Menu
+            open={Boolean(state.openMenu)}
+            anchorEl={state.openMenu}
+            onClose={CloseMenu}
+          >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem onClick={OpenModal}>Login</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Stack sx={{ margin: "70px auto", alignItems: "center" }}>
+        <Typography variant="h3">Welcome guest.</Typography>
+        <Button variant="outlined" onClick={OpenModal}>
+          Login
+        </Button>
+      </Stack>
+
+      <Modal
+        open={state.openModal}
+        onClose={CloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ModalBody />
+      </Modal>
+    </Container>
+  );
+}
+
+function ModalBody() {
+  const [register, setRegister] = useState(false);
+  const [formData, setFormData] = useState(
+    register
+      ? {
+          email: null,
+          username: null,
+          password: null,
+          password2: null,
+        }
+      : {
+          email: null,
+          password: null,
+        }
+  );
+
+  const registerFields = [
+    { name: "Email", type: "email" },
+    { name: "Username", type: "text" },
+    { name: "Password", type: "password" },
+    { name: "Password2", type: "password" },
+  ];
+  const loginFields = [
+    { name: "Email", type: "email" },
+
+    { name: "Password", type: "password" },
+  ];
+
+  function HandleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData((state) => ({
+      ...state,
+      [name.toLowerCase()]: value,
+    }));
+  }
+
+  function HandleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+  return (
+    <Stack
+      onSubmit={HandleSubmit}
+      component={"form"}
+      spacing={2}
+      sx={{
+        width: "450px",
+        padding: 5,
+        margin: "50px auto",
+        backgroundColor: "white",
+        borderRadius: 4,
+      }}
+    >
+      {register ? (
+        <>
+          <Stack>
+            <Typography variant="h4">Register</Typography>
+            <Typography variant="subtitle2">
+              Already a user?
+              <Button onClick={() => setRegister(false)}>Login</Button>now.
+            </Typography>
+          </Stack>
+          {registerFields.map((field, indx) => (
+            <TextField
+              key={indx}
+              name={field.name}
+              label={field.name}
+              type={field.type}
+              onChange={HandleChange}
+              required
+            />
+          ))}
+
+          <Button variant="contained" type="submit">
+            Register
+          </Button>
+        </>
+      ) : (
+        <>
+          <Stack>
+            <Typography variant="h4">Login</Typography>
+            <Typography variant="subtitle2">
+              Not a user?
+              <Button onClick={() => setRegister(true)}>Register</Button> now.
+            </Typography>
+          </Stack>
+
+          {loginFields.map((field, indx) => (
+            <TextField
+              key={indx}
+              name={field.name}
+              label={field.name}
+              type={field.type}
+              onChange={HandleChange}
+              required
+            />
+          ))}
+
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </>
+      )}
+    </Stack>
   );
 }
--- src/pages/auth/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function Auth() {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
-}
